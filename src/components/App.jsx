01303b63b44b0dddd58d14f3a29c6e4b4038bc04
HollowMax <maxholovatyi5@gmail.com>
2022-12-22 19:30:57 +0200

    Add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,43 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { Feedback } from './Feedback/Feedback';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export function App() {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const addFeedback = name => {
+    setFeedback(prevState => ({
+      ...prevState,
+      [name]: prevState[name] + 1,
+    }));
   };
 
-  addFeedback = name => {
-    this.setState(prevState => (prevState[name] += 1));
-  };
+  const countTotalFeedback = () => Object.values(feedback).reduce((acc, el) => acc + el, 0);
 
-  countTotalFeedback = () => Object.values(this.state).reduce((acc, el) => acc + el, 0);
+  const countPositiveFeedbackPercentage = total => parseInt((feedback.good / total) * 100);
 
-  countPositiveFeedbackPercentage = total => parseInt((this.state.good / total) * 100);
-
-  render() {
-    const total = this.countTotalFeedback();
-    const percent = this.countPositiveFeedbackPercentage(total);
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <Feedback options={Object.keys(this.state)} onLeaveFeedback={this.addFeedback} />
-        </Section>
-        <Section title="Statistics">
-          {total ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={total}
-              positivePercentage={percent}
-            />
-          ) : (
-            <Notification message="No feedback given" />
-          )}
-        </Section>
-      </>
-    );
-  }
+  const total = countTotalFeedback();
+  const percent = countPositiveFeedbackPercentage(total);
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <Feedback options={Object.keys(feedback)} onLeaveFeedback={addFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {total ? (
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={total}
+            positivePercentage={percent}
+          />
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </Section>
+    </>
+  );
 }
